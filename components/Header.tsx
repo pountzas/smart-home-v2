ca430f7eccceea56c0c89b3e3862bec45de79479
pountzas <nikos@pountzas.gr>
2024-08-30 20:01:13 +0300

    create basic structure header

---
+++ components/Header.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+const iconItems = [
+  {
+    name: "notifications",
+    icon: "notifications",
+    color: "black",
+    colorDark: "white"
+  },
+  {
+    name: "mail",
+    icon: "mail",
+    color: "black",
+    colorDark: "white"
+  },
+  {
+    name: "alerts",
+    icon: "alerts",
+    color: "black",
+    colorDark: "white"
+  }
+];
+
+function Header() {
+  return (
+    <div className="flex items-center justify-between">
+      logo
+      <div className="flex items-center space-x-2">
+        {iconItems.map((item) => (
+          <div key={item.name}>
+            <span>{item.name}</span>
+            <span>{item.icon}</span>
+          </div>
+        ))}
+        {/* <User /> */}
+      </div>
+    </div>
+  );
+}
+
+export default Header;
