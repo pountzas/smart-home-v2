c5b780d3d3543cc128e6fafbc9ff7884a0f14694
pountzas <nikos@pountzas.gr>
2024-08-30 20:00:51 +0300

    basic structure on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,29 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Smart Home",
-  description: "UI for controlling smart home devices and settings",
+  description: "UI for controlling smart home devices and settings"
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        <div className="flex items-start">
+          {/* <SideNav /> */}
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
