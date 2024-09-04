e25b26ab70ed7cb23bba414c5d755dd059bcd010
pountzas <nikos@pountzas.gr>
2024-09-04 22:08:40 +0300

    add UI atom

---
+++ atoms/uiAtom.ts
@@ -0,0 +1,16 @@
+import { atom } from "recoil";
+
+export const flowMiniVersionState = atom({
+  key: "flowMiniVersionState",
+  default: false
+});
+
+export const selectedModalCarState = atom({
+  key: "selectedModalCarState",
+  default: 1
+});
+
+export const modalOpenState = atom({
+  key: "modalOpenState",
+  default: false
+});
