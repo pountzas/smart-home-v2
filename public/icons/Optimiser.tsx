851b3a05c371c072dcc4a51406bf15fb3a6480a0
pountzas <nikos@pountzas.gr>
2024-09-13 22:46:42 +0300

    add optimiser icon

---
+++ public/icons/Optimiser.tsx
@@ -0,0 +1,18 @@
+function Optimiser({ fill = "#3F464B ", size = 18 }: IconProps) {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0.75 3.9H3V11.25H0.75V3.9ZM4.95 0.75H7.05V11.25H4.95V0.75ZM9.15 6.75H11.25V11.25H9.15V6.75Z"
+        fill={fill}
+      />
+    </svg>
+  );
+}
+
+export default Optimiser;
