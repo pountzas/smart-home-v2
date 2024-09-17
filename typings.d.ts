ff42abe5b29920d825949459b8344acfc60b75a1
pountzas <nikos@pountzas.gr>
2024-09-17 20:31:21 +0300

    add CarCardData type

--- typings.d.ts
+++ typings.d.ts
@@ -3,3 +3,23 @@ type IconProps = {
   fill?: string;
   fill2?: string;
 };
+
+type CarCardData = {
+  id: number;
+  car: string;
+  owner: string;
+  block1: string[];
+  block2: string[];
+  block3: string[];
+  block4: string[];
+  block5: string[];
+  emissionsData: number[];
+  costData: number[];
+  offPeakData: number[];
+  midPeakData: number[];
+  peakData: number[];
+  carBalance: number[];
+  schedules: [number, string, string, string, string][];
+  balanced: number[];
+  boosted: number[];
+};
