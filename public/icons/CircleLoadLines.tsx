fc453d664f0ec9d50ce9458f20cee0d2a08fb802
pountzas <nikos@pountzas.gr>
2024-09-17 20:17:17 +0300

    add CircleLoadLines icon

---
+++ public/icons/CircleLoadLines.tsx
@@ -0,0 +1,205 @@
+function CircleLoadLines() {
+  return (
+    <svg
+      width="301"
+      height="266"
+      viewBox="0 0 301 266"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M150.5 0.579837V22.6136"
+        stroke="#9EAAB3"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M299.925 151.337L278.086 151.337"
+        stroke="#9EAAB3"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M1.07471 151.337H22.9138"
+        stroke="#9EAAB3"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M174.451 14.2907L172.654 24.5692"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M197.676 20.569L194.137 30.3766"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M219.466 30.8206L214.294 39.8594"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M239.161 44.734L232.511 52.7293"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M256.162 61.8866L248.237 68.5954"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M269.952 81.757L260.993 86.9755"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M280.114 103.742L270.393 107.311"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M286.335 127.172L276.147 128.985"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M286.336 175.502L276.149 173.69"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M280.113 198.933L270.392 195.364"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M269.951 220.918L260.992 215.699"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M61.8393 257.941L68.4888 249.946"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M44.8384 240.788L52.763 234.08"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M31.0483 220.918L40.0071 215.699"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M20.8865 198.933L30.6074 195.364"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14.6654 175.502L24.8531 173.69"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14.6638 127.172L24.8515 128.985"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M20.887 103.742L30.608 107.311"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M31.0487 81.757L40.0076 86.9755"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M44.839 61.8866L52.7636 68.5954"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M61.839 44.734L68.4885 52.7293"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M81.5351 30.8206L86.7075 39.8594"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M103.325 20.569L106.863 30.3766"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M126.549 14.2907L128.345 24.5692"
+        stroke="#3F464B"
+        strokeWidth="1.15"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+
+      <path d="M225 243L208 225.5L210 223.5L227 241L225 243Z" fill="#BBC7CE" />
+    </svg>
+  );
+}
+
+export default CircleLoadLines;
