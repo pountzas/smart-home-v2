4d603d5660d767b9c3cca72f5cc9324a439846db
pountzas <nikos@pountzas.gr>
2024-09-18 20:21:34 +0300

    create dataUtils

---
+++ utils/dataUtils.ts
@@ -0,0 +1,129 @@
+const SolarData: SolarDataType[] = [
+  {
+    title: ["Events"],
+    details: ["25/12/2022"],
+    info: ["Schedule Name"],
+    status: ["Not Started"]
+  },
+  {
+    title: ["Events"],
+    details: ["25/12/2022"],
+    info: ["Schedule Name"],
+    status: ["In Progress"]
+  },
+  {
+    title: ["Events"],
+    details: ["25/12/2022"],
+    info: ["Schedule Name"],
+    status: ["Completed", "Attachment"]
+  }
+];
+
+const ChargeHistoryData: ChargeHistoryDataType[] = [
+  {
+    title: ["Tesla 3", "James"],
+    details: ["25/12/2022", "10.00"],
+    info: [
+      "12.11kWh",
+      "2h51m",
+      "145 g/kWh CO2",
+      "50.5 miles (est)",
+      "",
+      "£5.42"
+    ],
+    status: ["Fully Charged"]
+  },
+  {
+    title: ["Tesla S Plaid", "Nikos"],
+    details: ["25/12/2022", "10.00"],
+    info: [
+      "5.11kWh",
+      "0h51m",
+      "145 g/kWh CO2",
+      "10.5 miles (est)",
+      "",
+      "£2.42"
+    ],
+    status: ["Static Load"]
+  },
+  {
+    title: ["Tesla 3", "James"],
+    details: ["25/12/2022", "10.00"],
+    info: [
+      "12.11kWh",
+      "2h51m",
+      "145 g/kWh CO2",
+      "50.5 miles (est)",
+      "",
+      "£5.42"
+    ],
+    status: ["Dynamic Load"]
+  },
+  {
+    title: ["Tesla 3", "James"],
+    details: ["25/12/2022", "10.00"],
+    info: [
+      "12.11kWh",
+      "2h51m",
+      "145 g/kWh CO2",
+      "50.5 miles (est)",
+      "",
+      "£5.42"
+    ],
+    status: ["User Mode"]
+  },
+  {
+    title: ["Tesla 3", "James"],
+    details: ["25/12/2022", "10.00"],
+    info: [
+      "12.11kWh",
+      "2h51m",
+      "145 g/kWh CO2",
+      "50.5 miles (est)",
+      "",
+      "£5.42"
+    ],
+    status: ["Fully Charged"]
+  },
+  {
+    title: ["Tesla S Plaid", "Nikos"],
+    details: ["25/12/2022", "10.00"],
+    info: [
+      "5.11kWh",
+      "0h51m",
+      "145 g/kWh CO2",
+      "10.5 miles (est)",
+      "",
+      "£2.42"
+    ],
+    status: ["Static Load"]
+  },
+  {
+    title: ["Tesla 3", "Maria"],
+    details: ["25/12/2022", "10.00"],
+    info: [
+      "12.11kWh",
+      "2h51m",
+      "145 g/kWh CO2",
+      "50.5 miles (est)",
+      "",
+      "£5.42"
+    ],
+    status: ["Dynamic Load"]
+  },
+  {
+    title: ["Tesla 3", "James"],
+    details: ["25/12/2022", "10.00"],
+    info: [
+      "12.11kWh",
+      "2h51m",
+      "145 g/kWh CO2",
+      "50.5 miles (est)",
+      "",
+      "£5.42"
+    ],
+    status: ["User Mode"]
+  }
+];
+
+export { SolarData, ChargeHistoryData };
