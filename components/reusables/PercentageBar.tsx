45c4f08bb21638017b9b7b027680b7625f029906
pountzas <nikos@pountzas.gr>
2024-09-20 22:14:56 +0300

    create PercentageBar

---
+++ components/reusables/PercentageBar.tsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+
+type PercentageBarProps = {
+  startValue: number;
+  endValue: number;
+  title?: string;
+  topLeft?: string;
+  topRight?: string;
+  bottomLeft?: string;
+  bottomRight?: string;
+};
+
+function PercentageBar({
+  startValue,
+  endValue,
+  title,
+  topLeft,
+  topRight,
+  bottomLeft,
+  bottomRight
+}: PercentageBarProps) {
+  const [percentage, setPercentage] = useState<number>(0);
+
+  useEffect(() => {
+    const EmissionsPercentage = () => {
+      const calculate = (startValue / endValue) * 100;
+      return calculate.toFixed(0);
+    };
+    console.log(typeof EmissionsPercentage());
+    setPercentage(Number(EmissionsPercentage()));
+  }, [startValue, endValue]);
+
+  return (
+    <div className="w-full pt-6 space-y-2 text-sm">
+      <p>{title}</p>
+      <div className="flex items-center justify-between text-xs">
+        <p>{topLeft}</p>
+        <p>{topRight}</p>
+      </div>
+      <div className="h-2 bg-quaternary">
+        <div
+          style={{ width: `${percentage}%` }}
+          className="h-full bg-blue_custom"
+        ></div>
+      </div>
+      <div className="flex items-center justify-between text-xs">
+        <p>{bottomLeft}</p>
+        <p>{bottomRight}</p>
+      </div>
+    </div>
+  );
+}
+
+export default PercentageBar;
