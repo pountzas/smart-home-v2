af368ebe4eaf1e4d4b9b2f3a804d65f1b977f675
pountzas <nikos@pountzas.gr>
2024-09-30 20:13:56 +0300

    add logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+import SmartHome from "@/public/icons/SmartHome";
 import React from "react";
 const iconItems = [
   {
@@ -22,12 +23,12 @@ const iconItems = [
 
 function Header() {
   return (
-    <div className="flex items-center justify-between">
-      logo
+    <div className="flex items-center justify-between bg-primary">
+      <SmartHome />
       <div className="flex items-center space-x-2">
         {iconItems.map((item) => (
           <div key={item.name}>
-            <span>{item.name}</span>
+            {/* <span>{item.name}</span> */}
             <span>{item.icon}</span>
           </div>
         ))}
