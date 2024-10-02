ec3ef6fb0a14f662194ebe0e1dcf540850c4d043
pountzas <nikos@pountzas.gr>
2024-10-02 15:27:22 +0300

    add User icon

---
+++ public/icons/User.tsx
@@ -0,0 +1,12 @@
+function User({ fill = "#0094FF ", size = 20 }: IconProps) {
+  return (
+    <svg width={size} height={size} fill="none">
+      <path
+        fill={fill}
+        d="M10 0C4.475 0 0 4.475 0 10s4.475 10 10 10 10-4.475 10-10S15.525 0 10 0Z"
+      />
+    </svg>
+  );
+}
+
+export default User;
