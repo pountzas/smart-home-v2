74331c2074ee7e4b56638854d993115352faeb78
pountzas <nikos@pountzas.gr>
2024-10-03 11:20:19 +0300

    add search Component and User icon to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -32,15 +32,17 @@ const iconItems = [
 function Header() {
   return (
     <div className="flex items-center justify-between bg-primary">
-      <SmartHome />
-      <div className="flex items-center space-x-2">
+      <div className="flex items-center space-x-24">
+        <SmartHome />
+        <SearchComponent openVersion={true} />
+      </div>
+      <div className="flex items-center space-x-4 pr-2">
         {iconItems.map((item) => (
           <div key={item.name}>
-            {/* <span>{item.name}</span> */}
             <span>{item.icon}</span>
           </div>
         ))}
-        {/* <User /> */}
+        <User />
       </div>
     </div>
   );
