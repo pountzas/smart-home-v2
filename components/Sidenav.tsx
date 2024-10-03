657a9f0bbdb2c703e17d74420b9fbeee5dced962
pountzas <nikos@pountzas.gr>
2024-10-03 10:54:01 +0300

    create basic SideNav

---
+++ components/Sidenav.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Sidenav() {
+  return (
+    <div className="min-w-10 h-screen bg-primary border-r-[1px] border-tertiary">
+      <div>as</div>
+    </div>
+  );
+}
+
+export default Sidenav;
