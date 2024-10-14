c4d57af16869ccdef07db807c0e4ec7a3a06b834
pountzas <nikos@pountzas.gr>
2024-10-14 15:47:50 +0300

    ArrowCloseButton refactor logic

--- components/reusables/buttons/ArrowCloseButton.tsx
+++ components/reusables/buttons/ArrowCloseButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { modalOpenState } from "@/atoms/uiAtom";
+import { modalOpenState, rightmodalOpenState } from "@/atoms/uiAtom";
 import ArrowLeft from "@/public/icons/ArrowLeft";
 import { useRecoilState } from "recoil";
 
@@ -8,10 +8,18 @@ import { motion } from "framer-motion";
 
 function ArrowCloseButton() {
   const [modalOpen, setModalOpen] = useRecoilState(modalOpenState);
+  const [rightModalOpen, setRightModalOpen] =
+    useRecoilState(rightmodalOpenState);
+
+  const closeModals = () => {
+    setModalOpen(false);
+    setRightModalOpen(false);
+  };
+
   return (
     <motion.button
       whileTap={{ scale: 0.9 }}
-      onClick={() => setModalOpen(false)}
+      onClick={() => closeModals()}
       className="pl-8"
     >
       <ArrowLeft size={16} />
