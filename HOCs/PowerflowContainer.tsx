e62f5a748e863f3a69a5cd6a7e0a907f429284fd
pountzas <nikos@pountzas.gr>
2024-10-15 19:45:30 +0300

    add animation to PowerFlowContainer

--- HOCs/PowerflowContainer.tsx
+++ HOCs/PowerflowContainer.tsx
@@ -2,6 +2,7 @@
 
 import { flowMiniVersionState } from "@/atoms/uiAtom";
 import { useRecoilState } from "recoil";
+import { motion } from "framer-motion";
 
 type ChildrenProps = {
   children: React.ReactNode;
@@ -11,9 +12,14 @@ function PowerflowContainer({ children }: ChildrenProps) {
   const [flowMiniVersion, setFlowMiniVersion] =
     useRecoilState(flowMiniVersionState);
   return (
-    <div className={`${flowMiniVersion && "col-span-2 sm:col-span-1"} `}>
+    <motion.div
+      initial={{ width: flowMiniVersion ? "100%" : "100%" }}
+      animate={{ width: flowMiniVersion ? "100%" : "100%" }}
+      transition={{ duration: 0.4 }}
+      className={`${flowMiniVersion && "col-span-2 sm:col-span-1"} `}
+    >
       {children}
-    </div>
+    </motion.div>
   );
 }
 
