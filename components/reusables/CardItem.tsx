0c0791627e749ad55cb79cae475650d84ed9b929
pountzas <nikos@pountzas.gr>
2024-10-15 19:41:12 +0300

    CardItem minor UI changes

--- components/reusables/CardItem.tsx
+++ components/reusables/CardItem.tsx
@@ -21,14 +21,14 @@ function CardItem({
   return (
     <div className="h-[117px] flex justify-between py-4 pl-6 pr-9 border-b border-quaternary">
       <div className="flex flex-col items-start justify-evenly">
-        <h2 className="text-lg">{title}</h2>
-        <p className="text-">{timeAgo}</p>
-        <p className="text-sm text-textSecondary ">{description}</p>
+        <h2 className="text-sm">{title}</h2>
+        <p className="text-xs">{timeAgo}</p>
+        <p className="text-xs text-textSecondary ">{description}</p>
       </div>
       <div className="flex flex-col items-end justify-between py-2">
-        <p className={`${checkStatus(status)}`}>{status}</p>
+        <p className={`${checkStatus(status)} text-[10px]`}>{status}</p>
         {contributor && (
-          <p className="p-2 text-sm rounded-full bg-quaternary ">
+          <p className="p-2 text-[10px] rounded-full bg-quaternary ">
             {contributor}
           </p>
         )}
