8d0d92f382f8fb3708cdefb2a50a7371b7d0bc3c
pountzas <nikos@pountzas.gr>
2024-12-02 17:09:19 +0200

    create HomeIcon

---
+++ public/icons/HomeIcon.tsx
@@ -0,0 +1,18 @@
+function HomeIcon() {
+  return (
+    <svg
+      width="17"
+      height="18"
+      viewBox="0 0 17 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8.75 0L0.5 6V18H5.65625V11H11.8438V18H17V6L8.75 0Z"
+        fill="#C2C2CF"
+      />
+    </svg>
+  );
+}
+
+export default HomeIcon;
