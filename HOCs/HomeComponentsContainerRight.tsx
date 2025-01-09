f34ccdcbf5b28971d6fa490f236a5585ebe9d282
pountzas <nikos@pountzas.gr>
2025-01-09 12:46:27 +0200

    adjust HomeComponents Right container ui

--- HOCs/HomeComponentsContainerRight.tsx
+++ HOCs/HomeComponentsContainerRight.tsx
@@ -2,20 +2,26 @@
 import { flowMiniVersionState } from "@/atoms/uiAtom";
 import { useRecoilState } from "recoil";
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 type ChildrenProps = {
   children: React.ReactNode;
 };
 function HomeComponentsContainerRight({ children }: ChildrenProps) {
+  const [miniVersion, setMiniVersion] = useState(false);
   const [flowMiniVersion, setFlowMiniVersion] =
     useRecoilState(flowMiniVersionState);
+
+  useEffect(() => {
+    setMiniVersion(flowMiniVersion);
+  }, [flowMiniVersion]);
 
   return (
     <div
       className={`${
-        flowMiniVersion
-          ? "col-span-5 col-start-3 sm:col-span-3 md:col-span-4 lg:col-span-6 2xl:col-span-9"
-          : "2xl:col-span-2"
+        !miniVersion
+          ? "2xl:col-span-2"
+          : "col-span-5 col-start-3 sm:col-span-3 md:col-span-4 lg:col-span-6 2xl:col-span-9"
       } w-full grid overflow-y-scroll scrollbar-hide object-contain h-[65vh] lg:h-[72vh]`}
     >
       {children}
