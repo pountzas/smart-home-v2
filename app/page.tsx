efcbb4a9b8be6ee2007dc760251cfaa3a7c76201
pountzas <nikos@pountzas.gr>
2025-01-09 13:29:07 +0200

    test home ui

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ import Tickets from "@/components/tickets/Tickets";
 import Optimiser from "@/components/οptimiser/Optimiser";
 
 export default function Home() {
+  const miniVersion = false;
   return (
     <main className=" w-full">
       <div className="flex flex-col items-center justify-between w-full bg-primary">
@@ -21,9 +22,23 @@ export default function Home() {
           </div>
 
           <HomeComponentsContainer>
+            {/* <div
+            className={`w-full justify-between grid grid-rows-1 row-span-6 object-contain gap-4 px-4 text-textPrimary ${
+              !miniVersion
+                ? "lg:grid-cols-2 2xl:grid-cols-3"
+                : "grid-cols-7 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 2xl:grid-cols-10"
+            }`}
+          > */}
             <Powerflow />
 
             <HomeComponentsContainerRight>
+              {/* <div
+                className={`${
+                  !miniVersion
+                    ? "2xl:col-span-2"
+                    : "col-span-5 col-start-3 sm:col-span-3 md:col-span-4 lg:col-span-6 2xl:col-span-9"
+                } w-full grid overflow-y-scroll scrollbar-hide object-contain h-[65vh] lg:h-[72vh]`}
+              > */}
               <Graphs />
               <div className="grid grid-cols-1 gap-4 2xl:grid-cols-2">
                 <div ref={null}>
@@ -38,7 +53,9 @@ export default function Home() {
                 <Alarms />
                 <Tickets />
               </div>
+              {/* </div> */}
             </HomeComponentsContainerRight>
+            {/* </div> */}
           </HomeComponentsContainer>
         </div>
       </div>
